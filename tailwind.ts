60a996faf952eb423e18af9f183d09557e399bb3
12Gustavo21 <gustavo927318@gmail.com>
2026-05-06 15:12:10 -0300

    feat: implementing hero section

---
+++ tailwind.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['"DM Serif Display"', 'Georgia', 'serif'],
+        sans: ['"DM Sans"', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
